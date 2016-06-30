a36016c87f3960d0a7aad754c16e3454f5764074
Peter Perhac <perhac.peter@gmail.com>
2016-06-30 13:15:48 +0100

    attempting to fix shit

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: target/universal/stage/bin/play-getting-started -Dhttp.port=${PORT}
-console: target/universal/stage/bin/play-getting-started -main scala.tools.nsc.MainGenericRunner -usejavacp
+web: target/universal/stage/bin/fuel-meter -Dhttp.port=${PORT}
+console: target/universal/stage/bin/fuel-meter -main scala.tools.nsc.MainGenericRunner -usejavacp
---
+++ app/controllers/Application.scala
@@ -0,0 +1,33 @@
+package controllers
+
+
+import play.api.Play.current
+import play.api.db._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("Ahoj šéfe"))
+  }
+
+  def db = Action {
+    var out = ""
+    val conn = DB.getConnection()
+    try {
+      val stmt = conn.createStatement
+
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)")
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now())")
+
+      val rs = stmt.executeQuery("SELECT tick FROM ticks")
+
+      while (rs.next) {
+        out += "Read from DB: " + rs.getTimestamp("tick") + "\n"
+      }
+    } finally {
+      conn.close()
+    }
+    Ok(out)
+  }
+}
