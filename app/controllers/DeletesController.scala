6f86d45faeffeb4da90ff3b18ab3b22b48489aa7
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-01-04 13:58:47 +0000

    cutting out mongo crap from utility controller (overlooked)

--- app/controllers/DeletesController.scala
+++ app/controllers/DeletesController.scala
@@ -4,17 +4,14 @@ import javax.inject.Inject
 
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc._
-import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
+import repository.FuelMeterRepository
 
-class DeletesController @Inject()(val reactiveMongoApi: ReactiveMongoApi)
-  extends Controller with MongoController with ReactiveMongoComponents {
+class DeletesController @Inject()(repo: FuelMeterRepository) extends Controller {
 
   private val VReg = "vreg"
 
-  lazy val repo = new repository.RefuelMongoRepository(reactiveMongoApi)
-
   def deleteVehicle(implicit r: String) = Action.async {
-    _ => repo.removeByRegistration(r) map (_ => Redirect(routes.ReadingsController.index()).discardingCookies(DiscardingCookie("vreg")))
+    _ => repo.removeByRegistration(r) map (_ => Redirect(routes.ReadingsController.index()).discardingCookies(DiscardingCookie(VReg)))
   }
 
 }
