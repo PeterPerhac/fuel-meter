c6935125b6d0eaf50c82c03b9fab1ccd7b27f4c4
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-01-05 12:29:53 +0000

    using C3 with D3 to create charts (ditching chartist)

--- app/models/VehicleData.scala
+++ app/models/VehicleData.scala
@@ -1,7 +1,5 @@
 package models
 
-import java.sql.Timestamp
-import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 import play.api.libs.json.Json
@@ -11,7 +9,7 @@ import scala.math.BigDecimal.RoundingMode
 
 case class VehicleData(reg: String, avgC: BigDecimal, mpg: BigDecimal, costOfLitre: BigDecimal, readings: Seq[ReadingData])
 
-case class ReadingData(timestamp: Long, mi: Double, total: Int, litres: Double, cost: Double, avgC: BigDecimal, mpg: BigDecimal, costOfLitre: BigDecimal)
+case class ReadingData(date: String, mi: Double, total: Int, litres: Double, cost: Double, avgC: BigDecimal, mpg: BigDecimal, costOfLitre: BigDecimal)
 
 object ReadingData {
 
@@ -20,7 +18,7 @@ object ReadingData {
   val dateFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd")
 
   def apply(r: Reading): ReadingData = ReadingData(
-    Timestamp.valueOf(LocalDate.parse(r.date, dateFormat).atStartOfDay()).getTime,
+    r.date,
     r.mi, r.total, r.litres, r.cost,
     BigDecimal(r.litres / (r.mi * 0.0160934)).setScale(2, RoundingMode.HALF_UP),
     BigDecimal(r.mi / (r.litres / 4.54609188)).setScale(2, RoundingMode.HALF_UP),
