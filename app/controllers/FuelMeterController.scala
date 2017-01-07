1de1890a74a5eceb3aabbc4ef5990649a419418a
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-01-07 15:53:17 +0000

    cutting out common dependencies from controller, only inject what's needed

--- app/controllers/FuelMeterController.scala
+++ app/controllers/FuelMeterController.scala
@@ -1,7 +1,18 @@
 package controllers
 
-import play.api.i18n.I18nSupport
+import javax.inject.Inject
+
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.libs.ws.WSClient
 import play.api.mvc.Controller
 
 
-abstract class FuelMeterController extends Controller with I18nSupport with WSSupport
+abstract class FuelMeterController(val commonDependencies: CommonDependencies) extends Controller with I18nSupport with WSSupport {
+
+  protected lazy val ws = commonDependencies.ws
+  protected lazy val conf = commonDependencies.conf
+  implicit lazy val messagesApi = commonDependencies.messagesApi
+
+}
+
+final class CommonDependencies @Inject()(val ws: WSClient, val conf: play.api.Configuration, val messagesApi: MessagesApi)
--- app/controllers/ReadingsController.scala
+++ app/controllers/ReadingsController.scala
@@ -5,33 +5,29 @@ import javax.inject.Inject
 import models._
 import models.forms.ReadingForm.form
 import play.api.Logger
-import play.api.i18n.MessagesApi
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json._
-import play.api.libs.ws.WSClient
 import play.api.mvc._
 import repository.FuelMeterRepository
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
 
-class ReadingsController @Inject()(ws: WSClient, conf: play.api.Configuration, repo: FuelMeterRepository, val messagesApi: MessagesApi) extends FuelMeterController {
+class ReadingsController @Inject()(repo: FuelMeterRepository, ds: CommonDependencies) extends FuelMeterController(ds) {
 
   private val VReg = "vreg"
 
   lazy val lookupUrl = conf.underlying.getString("vehicle-lookup.service.url")
 
-  def vRegCookie(implicit r: String) = Cookie(VReg, r.filter(_.isLetterOrDigit).mkString, maxAge = Some(Int.MaxValue))
+  def vRegCookie(implicit r: String) = Cookie(name = VReg, value = r.filter(_.isLetterOrDigit).mkString, maxAge = Some(Int.MaxValue))
 
   def readings(implicit r: String): Future[Vector[Reading]] = repo.findAll(r)
 
   def uniqueRegistrations: Future[Seq[VehicleRecordSummary]] = repo.uniqueRegistrations()
 
   def vehicleDetails(implicit reg: String) = {
-    import scala.language.postfixOps
-    val url = s"$lookupUrl/$reg"
-    ws.url(url).withRequestTimeout(1 seconds).get() map toClassOf[Vehicle] recover {
-      case t => Logger.warn(s"Failed to retrieve vehicle details from URL: $url ; a${t.getMessage}")
+    ws.url(s"$lookupUrl/$reg").withRequestTimeout(1.seconds).get() map toClassOf[Vehicle] recover {
+      case t => Logger.warn(s"Failed to retrieve vehicle details. ${t.getMessage}")
         None
     }
   }
