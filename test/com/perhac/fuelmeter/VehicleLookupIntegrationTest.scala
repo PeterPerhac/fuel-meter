aff4e91e9923a1cb2a52d63960c88c2579a445d9
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-01-09 20:52:31 +0000

    adding integration test for vehicle-lookup service
    make sure the page is rendered with or without vehicle lookup service being available.

--- test/com/perhac/fuelmeter/VehicleLookupIntegrationTest.scala
+++ test/com/perhac/fuelmeter/VehicleLookupIntegrationTest.scala
@@ -1,37 +1,51 @@
 package com.perhac.fuelmeter
 
+import models.Vehicle
 import org.scalatestplus.play._
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.json._
+import play.api.mvc._
+import play.api.routing.sird._
 import play.api.test.Helpers._
 import play.api.test._
+import play.core.server.Server
 
-class RoutingTest extends FuelMeterTest with OneAppPerSuite {
+class VehicleLookupIntegrationTest extends FuelMeterTest with OneAppPerTest with RequestMethodExtractors {
 
-  "Routes" should {
+  val testReg: String = "TE5TR3G"
 
-    "send 404 on a bad request" in {
-      route(app, FakeRequest(GET, "/theres-nothing-mapped-to-this-url")).map(status) mustBe Some(NOT_FOUND)
-    }
 
-    "send 200 on a good request" in {
-      route(app, FakeRequest(controllers.routes.ReadingsController.index())) match {
-        case Some(homepage) =>
-          status(homepage) mustBe OK
-          contentAsString(homepage) must include("Fuel Meter")
-        case None => fail
+  "Vehicle page" should {
+
+    "still render fine even if vehicle lookup service is unreachable" in {
+      inside(route(app, FakeRequest(controllers.routes.ReadingsController.listHtml("NA08MYW")))) {
+        case Some(page) =>
+          status(page) mustBe OK
+          contentAsString(page) mustNot include("Red Star Forever")
       }
     }
 
-  }
 
-  "render the index page" in {
-    route(app, FakeRequest(GET, "/")) match {
-      case Some(homepage) =>
-        status(homepage) mustBe OK
-        contentType(homepage) mustBe Some("text/html")
-        contentAsString(homepage) must include("Here's how it works...")
-      case None => fail("request to /acme/capture returned nothing")
+    "render vehicle make and model when vehicle lookup service is reachable" in {
+
+      Server.withRouter() {
+        case GET(p"/v1/vehicles/$reg") => Action {
+          val veh = Vehicle(reg, "Star", "Forever", 1968, Some("Red"))
+          Results.Ok(Json.toJson(veh))
+        }
+      } { port =>
+        val serviceUrl = "vehicle-lookup.service.url"
+        inside(route(new GuiceApplicationBuilder().configure(Map(serviceUrl -> s"http://localhost:$port/v1/vehicles")).build(),
+          FakeRequest(controllers.routes.ReadingsController.listHtml("NA08MYW")))) {
+          case Some(page) =>
+            status(page) mustBe OK
+            contentAsString(page) must include("Red Star Forever")
+        }
+      }
     }
 
+
   }
+
 
 }
