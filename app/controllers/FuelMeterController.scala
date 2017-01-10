4f9f10cd23fb401904a182d0b532b39ba63092ac
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-01-10 23:56:12 +0000

    adding a custom action builder to allow enabling of Basic Auth

--- app/controllers/FuelMeterController.scala
+++ app/controllers/FuelMeterController.scala
@@ -2,10 +2,13 @@ package controllers
 
 import javax.inject.Inject
 
+import org.apache.commons.codec.binary.Base64.decodeBase64
 import play.api.Configuration
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.ws.WSClient
-import play.api.mvc.Controller
+import play.api.mvc.{Action, Controller}
+
+import scala.concurrent.Future
 
 
 abstract class FuelMeterController(val commonDependencies: CommonDependencies) extends Controller with I18nSupport with WSSupport {
@@ -13,6 +16,19 @@ abstract class FuelMeterController(val commonDependencies: CommonDependencies) e
   protected lazy val ws = commonDependencies.ws
   protected lazy val conf = commonDependencies.conf
   implicit lazy val messagesApi = commonDependencies.messagesApi
+
+  def BasicSecured[A](username: String, password: String)(action: Action[A]): Action[A] = Action.async(action.parser) { request =>
+    val submittedCredentials: Option[List[String]] = for {
+      authHeader <- request.headers.get("Authorization")
+      parts <- authHeader.split(' ').drop(1).headOption
+    } yield new String(decodeBase64(parts.getBytes)).split(":").toList
+
+    submittedCredentials.collect {
+      case u :: p :: Nil if u == username && p == password =>
+    }.map(_ => action(request)).getOrElse {
+      Future.successful(Unauthorized.withHeaders("WWW-Authenticate" -> """Basic realm="Secured Area""""))
+    }
+  }
 
 }
 
