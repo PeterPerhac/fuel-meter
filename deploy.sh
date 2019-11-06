0e66afcf259a9d12f3fb5fcd2360850602aa2151
Peter Perhac <perhac.peter@gmail.com>
2019-11-06 22:50:37 +0000

    sleep between deployments

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# sbt clean dist
+sbt clean dist
 
 DEVPRO_NO=1
 scp target/universal/fuel-meter-1.0-SNAPSHOT.zip opc@devpro$DEVPRO_NO:/home/opc &
@@ -11,5 +11,11 @@ scp target/universal/fuel-meter-1.0-SNAPSHOT.zip opc@devpro$DEVPRO_NO:/home/opc 
 wait $(jobs -p)
 
 ssh -t opc@devpro1 'unzip -o fuel-meter-1.0-SNAPSHOT.zip && sudo systemctl restart fuelmeter.service && exit'
+
+sleep 10
+curl http://devpro.perhac.com/fuelmeter
+
 ssh -t opc@devpro2 'unzip -o fuel-meter-1.0-SNAPSHOT.zip && sudo systemctl restart fuelmeter.service && exit'
+sleep 10
+curl http://devpro.perhac.com/fuelmeter
 
