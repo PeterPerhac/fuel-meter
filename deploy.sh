c1fca6ad8a154d154946beef8ec177996e6fbca7
Peter Perhac <perhac.peter@gmail.com>
2020-01-27 15:57:15 +0000

    more readings

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,9 @@
 
 sbt clean dist
 
-DEVPRO_NO=1
-scp target/universal/fuel-meter-1.0-SNAPSHOT.zip opc@devpro$DEVPRO_NO:/home/opc &
-
-DEVPRO_NO=2
-scp target/universal/fuel-meter-1.0-SNAPSHOT.zip opc@devpro$DEVPRO_NO:/home/opc &
-
-wait $(jobs -p)
-
-ssh -t opc@devpro1 'unzip -o fuel-meter-1.0-SNAPSHOT.zip && sudo systemctl restart fuelmeter.service && exit'
+scp target/universal/fuel-meter-1.0-SNAPSHOT.zip opc@devpro:/home/opc
+ssh -t opc@devpro 'unzip -o fuel-meter-1.0-SNAPSHOT.zip && sudo systemctl restart fuelmeter.service && exit'
 
 sleep 10
-curl http://devpro.perhac.com/fuelmeter
-
-ssh -t opc@devpro2 'unzip -o fuel-meter-1.0-SNAPSHOT.zip && sudo systemctl restart fuelmeter.service && exit'
-sleep 10
-curl http://devpro.perhac.com/fuelmeter
+curl http://devpro/fuelmeter
 
